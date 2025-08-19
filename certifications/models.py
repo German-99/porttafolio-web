19abf565a015ec3d37f90599ec122faa90865ec5
Adrian <adrian2009@live.com.mx>
2025-08-19 15:57:58 -0700

    first commit

---
+++ certifications/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.db.models.fields import CharField, URLField
+from django.db.models.fields.files import ImageField
+
+# Create your models here.
+class Certification(models.Model):
+    title = CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = ImageField(upload_to= 'certifications/images')
+    lenguages = models.ForeignKey("ProgramLenguage", on_delete=models.CASCADE)
+    url = URLField(blank=True)
+
+    def __str__(self):
+        return self.title 
+
+   
+
+
+class ProgramLenguage(models.Model):
+    choice = models.CharField(max_length=20)
+    icon = ImageField(upload_to= 'certifications/images/icons', null=True, blank=True)
+   
+    def __str__(self):
+        return self.choice 
+
